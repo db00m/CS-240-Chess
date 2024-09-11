434098930ceef679f0d99ba17c881fac8e3c8dac
david.bloomfield <david.s.bloomfield@gmail.com>
2024-09-10 20:07:29 -0600

    Implement Board Setup

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,15 @@ package chess;
  */
 public class ChessBoard {
 
+    private ChessPiece[][] boardArray = new ChessPiece[8][8];
+
+    public static void main(String [] args) {
+        ChessBoard board = new ChessBoard();
+        board.printBoard();
+    }
+
     public ChessBoard() {
-        
+        resetBoard();
     }
 
     /**
@@ -19,7 +26,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        boardArray[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
@@ -33,11 +40,52 @@ public class ChessBoard {
         throw new RuntimeException("Not implemented");
     }
 
+    public void printBoard() {
+        for (ChessPiece[] row : boardArray) {
+            StringBuilder rowString = new StringBuilder();
+            for (ChessPiece piece : row) {
+                rowString.append("|");
+                if (piece != null) {
+                    rowString.append(piece);
+                } else {
+                    rowString.append(" ");
+                }
+            }
+            rowString.append("|");
+            System.out.println(rowString);
+        }
+    }
+
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        // Create new blank board
+        boardArray = new ChessPiece[8][8];
+
+        for (ChessGame.TeamColor color : ChessGame.TeamColor.values()) {
+            // Add pawns to board
+            for (int i = 0; i < 8; i++) {
+                var pawn = new ChessPiece(color, ChessPiece.PieceType.PAWN);
+                addPiece(pawn.getStartingPosition(i), pawn);
+            }
+            // Add 2 count pieces
+            for (ChessPiece.PieceType type : ChessPiece.PieceType.values()) {
+                if (type == ChessPiece.PieceType.PAWN || type == ChessPiece.PieceType.KING || type == ChessPiece.PieceType.QUEEN) {
+                    continue;
+                } else {
+                    for (int i = 0; i < 2; i++) {
+                        var piece = new ChessPiece(color, type);
+                        addPiece(piece.getStartingPosition(i), piece);
+                    }
+                }
+            }
+            // Add King and Queen
+            var king = new ChessPiece(color, ChessPiece.PieceType.KING);
+            addPiece(king.getStartingPosition(0), king);
+            var queen = new ChessPiece(color, ChessPiece.PieceType.QUEEN);
+            addPiece(queen.getStartingPosition(0), queen);
+        }
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -10,7 +10,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    private final PieceType type;
+    private final ChessGame.TeamColor color;
+
+    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
+        this.type = type;
+        color = pieceColor;
     }
 
     /**
@@ -28,15 +33,82 @@ public class ChessPiece {
     /**
      * @return Which team this chess piece belongs to
      */
-    public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+    public ChessGame.TeamColor getColor() {
+        return color;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
-    public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+    public PieceType getType() {
+        return type;
+    }
+
+    public String toString() {
+        if (type == PieceType.KNIGHT && color == ChessGame.TeamColor.BLACK) {
+            return "N";
+        } else if (type == PieceType.KNIGHT) {
+            return "n";
+        }
+
+        if (color == ChessGame.TeamColor.BLACK) {
+            return type.toString().substring(0, 1).toUpperCase();
+        } else {
+            return type.toString().substring(0, 1).toLowerCase();
+        }
+    }
+
+    public ChessPosition getStartingPosition(int pieceIndex) {
+        return new ChessPosition(startingRow(), startingColumn(pieceIndex));
+    }
+
+    /**
+     *
+     * @param pieceIndex for pieces that have more than one, what is the current count.
+     * @return starting column of the piece (for use in ChessPosition)
+     */
+
+    private int startingColumn(int pieceIndex) {
+        return switch (type) {
+            case KING -> 4;
+            case QUEEN -> 3;
+            case BISHOP -> {
+                if (pieceIndex == 0) {
+                    yield 2;
+                } else {
+                    yield 5;
+                }
+            }
+            case KNIGHT -> {
+                if (pieceIndex == 0) {
+                    yield 1;
+                } else {
+                    yield 6;
+                }
+            }
+            case ROOK -> pieceIndex * 7; // 0 if index is 0 and 7 if index is 1
+            case PAWN -> pieceIndex;
+        };
+    }
+
+    /**
+     * Calculates the starting row of the piece.
+     * @return starting row (for use in ChessPosition)
+     */
+    private int startingRow() {
+        if (color == ChessGame.TeamColor.BLACK) {
+            if (type == PieceType.PAWN) {
+                return 6;
+            } else {
+                return 7;
+            }
+        } else {
+            if (type == PieceType.PAWN) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
     }
 
     /**
