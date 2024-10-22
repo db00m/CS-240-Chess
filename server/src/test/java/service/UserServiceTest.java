ad9918b6235b72ac8868554b9faf70ba8f464466
david.bloomfield <david.s.bloomfield@gmail.com>
2024-10-21 18:07:12 -0600

    Add user service tests

---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,55 @@
+package service;
+
+import dataaccess.UserDAO;
+import dataaccess.memorydao.MemoryUserDAO;
+import models.UserModel;
+import org.junit.jupiter.api.*;
+import requests.LoginRequest;
+import requests.RegisterRequest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    public static UserService service = new UserService();
+    public static UserDAO dao = new MemoryUserDAO();
+
+    public static UserModel user = new UserModel("username", "password", "fake@email.com");
+
+    @BeforeEach
+    void setup() {
+        dao.clearTable();
+    }
+
+    @Test
+    void registerUser() {
+        RegisterRequest request = new RegisterRequest("username", "password", "fake@email.com");
+        service.registerUser(request);
+
+        assertEquals(user, dao.getUserByUsername("username"));
+        assertThrows(ValidationException.class, () -> service.registerUser(request));
+    }
+
+    @Test
+    void loginUser() {
+        dao.add(user);
+
+        LoginRequest validRequest = new LoginRequest("username", "password");
+        LoginRequest invalidRequest = new LoginRequest("username", "wrong");
+
+        assertNotNull(service.loginUser(validRequest));
+        assertThrows(ValidationException.class, () -> service.loginUser(invalidRequest));
+    }
+
+    @Test
+    void logoutUser() {
+        dao.add(user);
+
+        LoginRequest validRequest = new LoginRequest("username", "password");
+        UUID token = service.loginUser(validRequest);
+
+        assertDoesNotThrow(() -> service.logoutUser(token));
+    }
+}
