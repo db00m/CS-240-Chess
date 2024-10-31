6510f01f6bca1b77bad52cf1c6754c0b4ed641b2
david.bloomfield <david.s.bloomfield@gmail.com>
2024-10-30 19:30:28 -0600

    add  method to SQLChessGameDAO

--- server/src/main/java/dataaccess/ChessGameDAO.java
+++ server/src/main/java/dataaccess/ChessGameDAO.java
@@ -5,7 +5,7 @@ import models.ChessGameModel;
 import java.util.Collection;
 
 public interface ChessGameDAO {
-    public int add(String gameName);
+    public int add(String gameName) throws DataAccessException;
     public ChessGameModel getById(int id) throws DataAccessException;
     public Collection<ChessGameModel> getAll();
     public void clear();
--- server/src/main/java/dataaccess/SQLChessGameDAO.java
+++ server/src/main/java/dataaccess/SQLChessGameDAO.java
@@ -1,11 +1,18 @@
 package dataaccess;
 
+import chess.ChessGame;
 import models.ChessGameModel;
+import serialize.ObjectSerializer;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 import java.util.List;
 
 public class SQLChessGameDAO implements ChessGameDAO {
+
+    Connection conn = DatabaseManager.getConnection();
 
     public static String CREATE_TABLE = """
             CREATE TABLE IF NOT EXISTS chess_games(
@@ -13,16 +20,40 @@ public class SQLChessGameDAO implements ChessGameDAO {
                 name VARCHAR(255) NOT NULL,
                 white_user_id INT,
                 black_user_id INT,
-                game_data VARCHAR(1000),
+                game_data VARCHAR(5000),
                 PRIMARY KEY (id),
                 FOREIGN KEY (white_user_id) REFERENCES users(id),
                 FOREIGN KEY (black_user_id) REFERENCES users(id)
             );
             """;
 
+    public SQLChessGameDAO() throws DataAccessException {
+    }
+
+    public static void main(String [] args) throws DataAccessException {
+        var dao = new SQLChessGameDAO();
+        dao.add("newGame");
+    }
+
     @Override
-    public int add(String gameName) {
-        return 0;
+    public int add(String gameName) throws DataAccessException {
+        var serializer = new ObjectSerializer();
+        String gameData = serializer.toJson(new ChessGame());
+        var statement = "INSERT INTO chess_games (name, game_data) VALUES (?, ?)";
+
+        try (var preparedStatement = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, gameName);
+            preparedStatement.setString(2, gameData);
+            preparedStatement.executeUpdate();
+            var rs = preparedStatement.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        throw new DataAccessException("Game was not created");
     }
 
     @Override
